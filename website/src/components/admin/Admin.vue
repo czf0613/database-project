8d4584e5f3ec33f37ca9122907760911e0556608
czf <1354016594@qq.com>
2020-12-24 19:55:35 +0800

    添加管理员页面

---
+++ website/src/components/admin/Admin.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>管理员</p>
+</template>
+
+<script>
+export default {
+  name: "Admin"
+}
+</script>
+
+<style scoped>
+
+</style>
--- website/src/components/home/HomePage.vue
+++ website/src/components/home/HomePage.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>主页面</h1>
+  <div>
+    <h1>主页面</h1>
+    <!--还要写一个学生界面，这里暂时没有写 -->
+    <Admin/>
+  </div>
 </template>
 
 <script>
+import Admin from "@/components/admin/Admin"
+
 export default {
-  name: "HomePage"
+  name: "HomePage",
+  components: {
+    Admin
+  }
 }
 </script>
 
